459b8d00247f019d8caa2caf882356a737e0626a
Ricardo Oliveira <ricardooliveira@unknowne231cff33e99.lan>
2026-09-24 10:08:22 +0100

    Update author

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "Diogo Cachetas"
-__credits__ = ["Diogo Cachetas", "João Capela", "João Correia", "Fernando Cruz", "Vítor Pereira"]
-__license__ = "Apache License 2.0"
-__version__ = "0.0.1"
-__maintainer__ = "Diogo Cachetas"
-__email__ = "diogo.cachetas@inl.int"
+__author__ = "Ricardo Oliveira" 
+__credits__ = ["Diogo Cachetas", "João Capela", "João Correia", "Fernando Cruz", "Vítor Pereira"] 
+__license__ = "Apache License 2.0" 
+__version__ = "0.0.1" 
+__maintainer__ = "Ricardo Oliveira" 
+__email__ = "pg59763@uminho.pt" 
 
 
 
